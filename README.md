508216d19d75f2e8af4a4bc24580372eb513fbc6
Nishi Shishir <psyns8@nottingham.ac.uk>
2024-03-13 14:34:39 +0000

    Updated README with how to use + information on packages Code now lets you run an image on a trained dataset, which can then identify people in that image

--- README.md
+++ README.md
@@ -11,3 +11,30 @@ Repo for Facial Recognition
 4. Pillow
 
 To download dlib and face-recognition, your local device needs Visual Studio C++ version to also be installed
+
+### Repository
+
+The code will ask you to install face_recognition_models from the following repository:https://github.com/ageitgey/face_recognition_models
+
+To install using pip: 
+
+    pip install git+https://github.com/ageitgey/face_recognition_models
+
+When running the code, it might give you an error even after you install the git repository. To fix this, run:
+
+    pip install setuptools
+
+The repository should work afterwards.
+
+## How to use
+
+Un-comment out the initial block of code to create the sub-folders needed - these will be training/output/validation
+
+Once these are created, add any images wanted into the training folder - this folder can have sub-folders if needed, and the code should read them accurately.
+
+You then want to run the encode_known_faces() function - this only needs to be run once at the start to train the models. The encodings for the faces will be stored afterwards. If more images are added, the function will have to be rerun.
+
+
+
+
+
--- face_recogniser/facialRecognition/detector.py
+++ face_recogniser/facialRecognition/detector.py
@@ -1,11 +1,15 @@
 # detector.py
-
 from pathlib import Path
+from collections import Counter
+from PIL import Image, ImageDraw
 
 import face_recognition
 import pickle
 
 DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
+BOUNDING_BOX_COLOUR = "blue"
+TEXT_COLOUR = "white"
+
 
 """
 
@@ -13,7 +17,7 @@ IMPORTANT: UN-COMMENT THE FOLLOWING CODE TO MAKE RELEVANT DIRECTORIES
 Creates Directories
 
 Path("training").mkdir(exist_ok=True)
-Path("output").mkdir(exist_ok=True)
+Path("output").mkdir(exist_ok=True)o
 Path("validation").mkdir(exist_ok=True)
 
 """
@@ -50,4 +54,71 @@ def encode_known_faces(
     with encodings_location.open(mode="wb") as f:
         pickle.dump(name_encodings, f)
 
-encode_known_faces()
+#NS - Recognises faces in images that don't have a label
+def recognise_faces(image_location: str, model: str = "hog", encodings_location: Path = DEFAULT_ENCODINGS_PATH,) -> None:
+    with encodings_location.open(mode="rb") as f:
+        #NS - Create test image
+        loaded_encodings = pickle.load(f)
+
+    input_image = face_recognition.load_image_file(image_location)
+
+    #NS - Detect faces in the input image and get their encodings
+    input_face_locations = face_recognition.face_locations(input_image, model=model)
+    input_face_encodings = face_recognition.face_encodings(input_image, input_face_locations)
+
+    #NS - Creates pillow image object from loaded input image
+    pillow_image = Image.fromarray(input_image)
+
+    #NS - Helps draw bounding box around detected faces
+    draw = ImageDraw.Draw(pillow_image)
+
+    #NS - iterate through locations and encodings in parallel
+    for bounding_box, unknown_encoding in zip(input_face_locations, input_face_encodings):
+        name = _recognise_face(unknown_encoding, loaded_encodings)
+        #NS - Checks if the name has a match - if nothing is returned, change the name to Unknown
+        if not name:
+            name = "Unknown"
+
+        #NS - shows name and bounding box size
+        #print(name, bounding_box)
+
+        _display_face(draw, bounding_box, name)
+
+    #NS - Housekeeping for Pillow
+    del draw
+    pillow_image.show()
+
+#NS - Takes the unknown encoding and loaded encodings, and makes a comparison between the unkown encoding
+# and the loaded encodings
+#NS - Will return the most likely match
+def _recognise_face(unknown_encoding, loaded_encodings):
+    #NS - compare_faces returns a list of true and false values for each loaded encoding
+    #NS - The indicies of this list are equal to those of the loaded encodings
+    boolean_matches = face_recognition.compare_faces(loaded_encodings["encodings"], unknown_encoding)
+
+    #NS - Tracks how many votes each potential match has
+    #NS - The unknown face is compared to every known face that you have encodings for
+    #   - Each match counts as a vote for the person with the known face
+    votes = Counter(
+        name
+        for match, name in zip(boolean_matches, loaded_encodings["names"])
+        if match
+    )
+    if votes:
+        return votes.most_common(1)[0][0]
+
+def _display_face(draw, bounding_box, name):
+    #NS - Unpack bounding_box into four parts, and draw rectangle around detected face
+    top, right, bottom, left = bounding_box
+    draw.rectangle(((left, top),(right,bottom)),outline=BOUNDING_BOX_COLOUR)
+
+    #NS - Determines bounding-box for caption
+    text_left, text_top, text_right, text_bottom = draw.textbbox((left,bottom), name)
+    draw.rectangle(((text_left, text_top), (text_right, text_bottom)), fill="blue", outline="blue")
+    draw.text((text_left, text_top), name, fill="white")
+'''
+IMPORTANT: ONLY RUN THE FOLLOWING LINE ONE TIME (unless more pictures are added)
+'''
+#encode_known_faces()
+
+recognise_faces("unknown.jpg")
